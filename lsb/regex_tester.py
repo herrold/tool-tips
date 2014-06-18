fe84480a9ed8d81758e0bca53feaf38b4fcbf34c
Russ Herrold <herrold@owlriver.com>
2014-06-18 09:42:51 -0400

    regex testing harness for mats testing

---
+++ lsb/regex_tester.py
@@ -0,0 +1,31 @@
+import re
+#	make sure greedy is working with the first set
+# long_string = 'GtkAction:::*(* get_action):(GtkActionGroup *action_group, const gchar * action_name)'
+long_string = 'GtkAction *(* get_action) (GtkActionGroup *action_group, const gchar * action_name)'
+
+# split on pattern, being greedy on whitespace ( or alternative delimiter)
+# my_pattern = r'[:]+'
+#	we want to spread this out for analysis
+
+#
+# my_pattern = re.compile(r"""
+# [:]+
+# """, re.VERBOSE)
+
+#
+# my_pattern = re.compile(r"""
+# [\s]+
+#""", re.VERBOSE)
+
+#	
+my_pattern = re.compile(r"""
+(.+\s+\*?)
+(\(\*.+)
+""", re.VERBOSE)
+
+my_array = re.split(my_pattern, long_string)
+
+for i in my_array:
+  print " token> "+i
+
+print "==========="
