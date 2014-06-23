25d5d2187b739508435eaa1373bbcf3e62fb2bb0
Russ Herrold <herrold@owlriver.com>
2014-06-23 19:02:37 -0400

    add audit tools for git.centos.org assertions off package tree

---
+++ clefos/gitpull.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+#	gitpull.sh
+#		$Id: gitpull.sh,v 1.7 2014/06/23 23:00:13 herrold Exp herrold $
+#	
+#	Copyright (c) R P Herrold info@owlriver.com
+#	reports to: info@owlriver.com	
+#	license: GPLv3+
+#	master lives in: /home/herrold/clefos/git-centos.org/
+#		as: /home/herrold/clefos/git-centos.org/gitpull.sh
+#
+PATH='/bin:/usr/bin:/usr/sbin:/sbin'
+MYNAME=`basename $0`
+#
+YMD=`date +%Y%m%d`
+#
+DRIVER="DRIVER"
+#	this file is hand generated using output from
+#	spot-branches.sh as to viable branches
+#
+DIRECT="/home/herrold/clefos/git-centos.org"
+#
+cd
+cd ${DIRECT}
+[ ! -e ${DRIVER} ] && {
+	echo "error: ${MYNAME} cannot see: ${DRIVER} in cwd: ${DIRECT} -- bailing " 1>&2
+	exit 1
+	}
+for i in `grep -v ^# ${DRIVER}`; do
+#
+#	expected directory form ends .git$, and 
+#	starts after the next prior '/'
+#
+#	we can safely freshen with a new pull with a simple:
+#		rm -rf (whatever)
+#
+	DIRN=`basename $i .git`
+#
+#	emit some URLs for the follow along at home crowd
+	echo "trying: ${DIRN} " 1>&2
+	echo "URL: $i " 1>&2
+# 	looks like: 
+# https://git.centos.org/git/sig-cloud-instance/build-instance.git
+#	summary URL looks like: 
+# https://git.centos.org/summary/sig-cloud-instance!build-instance.git
+	SUMMARY=`echo "$i" | sed -e "s@org/git/sig@org/summary/sig@g" \
+		-e "s@nstance/@nstance!@g" -e "s@core/@core!@g"`
+	echo "SUMMARY: ${SUMMARY} " 1>&2
+#
+	[ ! -d ${DIRN} ] && git clone $i
+	[ -e ${DIRN} ] && {
+		cd ${DIRN}
+		git pull
+		cd ..
+		cd
+		}
+done
+#
---
+++ clefos/spot-branches.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#	spot-branches.sh
+#		$Id: spot-branches.sh,v 1.2 2014/06/23 23:00:13 herrold Exp herrold $
+#
+#	
+#	Copyright (c) R P Herrold info@owlriver.com
+#	reports to: info@owlriver.com	
+#	license: GPLv3+
+#	master lives in: /home/herrold/clefos/git-centos.org/
+#		as: /home/herrold/clefos/git-centos.org/spot-branches.sh
+#
+PATH='/bin:/usr/bin:/usr/sbin:/sbin'
+MYNAME=`basename $0`
+#
+YMD=`date +%Y%m%d`
+#
+DIRECT="/home/herrold/clefos/git-centos.org"
+URL="https://git.centos.org/"
+#
+cd
+cd ${DIRECT}
+#
+lynx -dump ${URL} | grep "sig\-" | \
+	awk -F";" {'print $1'} | awk {'print $2'} | \
+	sed -e "s@\.git/@.git @g" | awk {'print $1'} | \
+	grep "/tree/" | \
+	sort | uniq
+#	or: summary/sig vs tree ?
+#
