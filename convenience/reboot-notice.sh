b85861f485ab2897169e42ac7b768c40a3df586b
R P Herrold <herrold@owlriver.com>
2015-01-14 10:51:56 -0500

    add which, mailx installers for the s390x

--- convenience/reboot-notice.sh
+++ convenience/reboot-notice.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 #
 #	reboot-notice.sh
+#		Version: $Id: reboot-notice.sh,v 1.13 2015/01/14 15:49:08 herrold Exp herrold $
 #
 #	formerly: proprietary
 #	License: GPLv3+
 #	info@owlriver.com
 #	Copyright (c) 2010 R P Herrold, Columbus OH
-#		$Id: reboot-notice.sh,v 1.7 2014/07/15 21:40:29 herrold Exp herrold $
 #
 #	UDP 'phone in' an update when a reboot happens, optionally
 #	with the -m option
 #
 #	add to crontab on remote unit:
 #	@reboot		/root/bin/reboot-notice.sh > /dev/null 2>&1 
+#		- or, with mailer -
+#	@reboot         /root/bin/reboot-notice.sh -m > /dev/null 2>&1
 #
 #	see: man 5 crontab, toward the bottom
 #
 PATH='/usr/sbin:/sbin:/usr/bin:/bin:~/bin/'
+MYNAME=`basename $0`
 #
 #	some local variables ...
 CENTRAL="http://www.owlriver.com/setup/"
-RECIPIENT="FIXME@example.com"
+RECIPIENT="herrold@owlriver.com"
 LIMIT=10
+VERSION=`grep "Version:" $0 | grep "${MYNAME}"  | \
+	awk -F"," {'print $2'} | awk {'print $2'} `
 #
 #	and some boilerplate
 DEBUG=""
@@ -78,15 +83,35 @@ cd
 #
 MYNAME=`basename $0 2> /dev/null`
 MYHOST=`hostname 2> /dev/null | tr 'A-Z' 'a-z'  2> /dev/null `
-export WEBARG="hostname=${MYHOST}&amp;progname=${MYNAME}"
+export WEBARG="hostname=${MYHOST}&amp;progname=${MYNAME}&amp;version=${VERSION}"
+#
+#	make sure we have what we need:
+WHICH=`which which 2> /dev/null`
+[ "x${WHICH}" = "x" ] && {
+	yum clean all 2> /dev/null 
+	yum -q -y --skip-broken install which 
+	}
+MAIL=`which mail 2> /dev/null`
+[ "x${MAIL}" = "x" ] && {
+	yum clean all 2> /dev/null
+	yum -q -y --skip-broken install mailx
+	}
+MAIL=`which mail 2> /dev/null`
+#
 DELAY=`date 2> /dev/null | md5sum - 2> /dev/null | tr -d 'a-fA-F' 2> /dev/null | cut -c 1-2 2> /dev/null `
 [ "x${DEBUG}" != "x" ] &&  export DELAY="0 "
 [ "x${DEBUG}" != "x" ] && {
-	echo "DEBUG:  ${DEBUG} " 1>&2
-	echo "QUIET:  ${QUIET} " 1>&2
-	echo "YES:    ${YES} "   1>&2
-	echo "DELAY:  ${DELAY} " 1>&2
-	echo "NETURL: ${CENTRAL}?${WEBARG} " 1>&2
+	echo "DEBUG:    ${DEBUG}   " 1>&2
+	echo "MYNAME:   ${MYNAME}  " 1>&2
+	echo "VERSION:  ${VERSION} " 1>&2
+	echo "QUIET:    ${QUIET}   " 1>&2
+	echo "YES:      ${YES}     " 1>&2
+	echo "DELAY:    ${DELAY}   " 1>&2
+	echo "                     " 1>&2
+	echo "CENTRAL:  ${CENTRAL} " 1>&2
+	echo "WEBARG:   ${WEBARG}  " 1>&2
+	echo "                     " 1>&2
+	echo "NETURL:   ${CENTRAL}?${WEBARG} " 1>&2
 	}
 #
 #	wait to let the default route go live; we randomly wait
@@ -120,9 +145,19 @@ HASIP=` curl ${QUIET} -o - ${CENTRAL}?${WEBARG} 2> /dev/null | \
 #
 #	optionally, SEND_EMAIL handler (option -m )
 [ "x${SEND_EMAIL}" != "x" ] && {
-	( echo "reboot notice ${MYHOST} via: ${MYNAME} " ; \
-	echo "My ip: ${HASIP} " ) | \
-		mail -s "reboot notice ${MYHOST} via: ${MYNAME} " \
+	( echo "reboot notice ${MYHOST} via: ${MYNAME} " ; 	\
+	echo "Version: ${VERSION} " ; 				\
+	echo "#" ;						\
+	hostname ;						\
+	echo "#" ;						\
+	echo "My ip: ${HASIP} " ; 				\
+	echo "#" ;						\
+	ip addr show ;						\
+	echo "#" ;						\
+	ifconfig ;						\
+	echo "#" ;						\
+		) | 						\
+		${MAIL} -s "reboot notice ${MYHOST} via: ${MYNAME} " \
 		${RECIPIENT} > /dev/null 1>&2
 	}
 #
